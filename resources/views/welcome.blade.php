af1f03637770be5aaa26d3b94d69a8dd90783ab7
itiwariarun <itiwariarun438@gmail.com>
2022-09-25 16:55:56 +0530

    improved pagination

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
 
+
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 
     @vite('resources/js/app.js')
